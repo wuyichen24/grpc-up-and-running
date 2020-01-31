a03d9665cdce824e9b723ae69bde2baacbc418c2
wuyichen24 <wuyichen24@gmail.com>
2020-01-31 01:07:17 -0700

    add service implementation

---
+++ productinfo/service/productinfo_service.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	pb "grpc-up-and-running/productinfo/service/ecommerce"
+
+	"github.com/gofrs/uuid"
+)
+
+// server is used to implement ecommerce/product_info.
+type server struct {
+	productMap map[string]*pb.Product
+}
+
+// AddProduct implements ecommerce.AddProduct
+func (s *server) AddProduct(ctx context.Context,
+	in *pb.Product) (*pb.ProductID, error) {
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal,
+			"Error while generating Product ID", err)
+	}
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+	s.productMap[in.Id] = in
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+
+}
+
+// GetProduct implements ecommerce.GetProduct
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
+	value, exists := s.productMap[in.Value]
+	if exists {
+		return value, status.New(codes.OK, "").Err()
+	}
+	return nil, status.Errorf(codes.NotFound, "Product does not exist.", in.Value)
+}
